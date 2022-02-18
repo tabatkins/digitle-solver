9150583863120d64d55c45ca13a21d149c026bad
Tab Atkins-Bittner <jackalmage@gmail.com>
2022-02-18 11:39:19 -0800

    Replace the hand-rolled code that handled exactly 6 tiles with generic code that'll do any number.

--- digitle.py
+++ digitle.py
@@ -37,60 +37,31 @@ def makeOps(val1: MathNode, val2: MathNode) -> Generator["MathOp", None, None]:
 
 def genExprs(nums: Sequence[int]) -> Generator[MathNode, None, None]:
     # Initial list gives the size-1 results
-    exprs: List[List["MathOp"]] = [[], [], [], [], [], [], []]
+    exprs: List[List["MathNode"]] = [[], list(nums)]
 
-    nums = list(nums)
-    yield from nums
+    yield from exprs[1]
 
-    # combine them to get the size-2 results
-    for lhs, rhs in itertools.combinations(nums, 2):
-        exprs[2].extend(makeOps(lhs, rhs))
-    yield from exprs[2]
+    for exprSize in range(2, len(exprs[1]) + 1):
+        exprs.append([])
+        for lhsSize in range(1, exprSize // 2 + 1):
+            rhsSize = exprSize - lhsSize
+            pairs: Iterator[Tuple[MathNode, MathNode]]
+            if lhsSize == rhsSize:
+                pairs = itertools.combinations(exprs[lhsSize], 2)
+            else:
+                pairs = itertools.product(exprs[lhsSize], exprs[rhsSize])
+            for op1, op2 in pairs:
+                if tiles(op1) & tiles(op2):
+                    continue
+                exprs[exprSize].extend(makeOps(op1, op2))
+        yield from exprs[exprSize]
 
-    # combine the 1 and 2 to get size-3,
-    # but check for shared tiles
-    for tile, op in itertools.product(nums, exprs[2]):
-        if tile in list(op.tiles):
-            continue
-        exprs[3].extend(makeOps(tile, op))
-    yield from exprs[3]
 
-    # combine 1 and 3, and 2 and 2, to get size 4
-    for tile, op in itertools.product(nums, exprs[3]):
-        if tile in list(op.tiles):
-            continue
-        exprs[4].extend(makeOps(tile, op))
-    for op1, op2 in itertools.combinations(exprs[2], 2):
-        if set(op1.tiles) & set(op2.tiles):
-            continue
-        exprs[4].extend(makeOps(op1, op2))
-    yield from exprs[4]
-
-    # 1 and 4, 2 and 3 to get size 5
-    for tile, op in itertools.product(nums, exprs[4]):
-        if tile in list(op.tiles):
-            continue
-        exprs[5].extend(makeOps(tile, op))
-    for op1, op2 in itertools.product(exprs[2], exprs[3]):
-        if set(op1.tiles) & set(op2.tiles):
-            continue
-        exprs[5].extend(makeOps(op1, op2))
-    yield from exprs[5]
-
-    # finally, 1/5, 2/4, and 3/3 for size 6
-    for tile, op in itertools.product(nums, exprs[5]):
-        if tile in list(op.tiles):
-            continue
-        exprs[6].extend(makeOps(tile, op))
-    for op1, op2 in itertools.product(exprs[2], exprs[4]):
-        if set(op1.tiles) & set(op2.tiles):
-            continue
-        exprs[6].extend(makeOps(op1, op2))
-    for op1, op2 in itertools.combinations(exprs[3], 2):
-        if set(op1.tiles) & set(op2.tiles):
-            continue
-        exprs[6].extend(makeOps(op1, op2))
-    yield from exprs[6]
+def tiles(op: "MathNode") -> Set[int]:
+    if isinstance(op, int):
+        return set([op])
+    else:
+        return set(op.tiles)
 
 
 @dataclasses.dataclass
@@ -185,7 +156,7 @@ if __name__ == "__main__":
         description=f"Tries to solve today's digitle: https://c.eev.ee/digitle/"
     )
     argparser.add_argument("target", help="The target value.")
-    argparser.add_argument("numbers", nargs=6, help="The 6 starting number tiles.")
+    argparser.add_argument("numbers", nargs="+", help="The starting number tiles.")
 
     args = argparser.parse_args()
     solve(int(args.target), [int(x) for x in args.numbers])
